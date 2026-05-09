e1141213b038e16d547a4be22c1846d3864fb85e
haroldsantivanez-ship-it <harold.santivanez@tecsup.edu.pe>
2026-05-09 00:26:00 -0500

    add viewsets with search filter for Equipo and Jugador

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Equipo, Jugador
+from .serializers import EquipoSerializer, JugadorSerializer
 
-# Create your views here.
+
+class EquipoViewSet(viewsets.ModelViewSet):
+    queryset         = Equipo.objects.all()
+    serializer_class = EquipoSerializer
+    filter_backends  = [filters.SearchFilter]
+    search_fields    = ['nombre', 'ciudad', 'categoria']
+
+
+class JugadorViewSet(viewsets.ModelViewSet):
+    queryset         = Jugador.objects.all()
+    serializer_class = JugadorSerializer
+    filter_backends  = [filters.SearchFilter]
+    search_fields    = ['nombre', 'posicion']
